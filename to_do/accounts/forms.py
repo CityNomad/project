bd75cf138e14f9e099c964983f6f6d7528b0e67e
CityNomad <romancheva@hotmail.com>
2023-01-04 19:55:16 +0600

    forms, views, models, urls and templates were created in accounts app. Base html template was created in webapp application. Static directory was succesfully created in webapp.'

---
+++ to_do/accounts/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.core.validators import validate_email
+
+
+class MyUserCreationForm(UserCreationForm):
+    email = forms.EmailField(max_length = 200, required = True, validators=[validate_email])
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if cleaned_data['last_name'] == '':
+            if cleaned_data['first_name'] == '':
+                raise forms.ValidationError("Заполните одно из полей - Имя или Фамилию")
+            return cleaned_data
+        return cleaned_data
+
+        # last_name = self.cleaned_data['last_name']
+        # print(last_name)
+        # first_name = self.cleaned_data['first_name']
+        # print(first_name)
+        # if last_name == '':
+        #     if first_name == '':
+        #         raise forms.ValidationError("Заполните одно из полей - Имя или Фамилию")
+        #     return self.cleaned_data
+        # return self.cleaned_data
+
+    class Meta(UserCreationForm.Meta):
+        fields = ['username', 'password1', 'password2', 'first_name', 'last_name', 'email']
--- to_do/accounts/views.py
+++ to_do/accounts/views.py
@@ -1,3 +1,70 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView, DetailView
+from accounts.forms import MyUserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+from django.contrib.auth.mixins import LoginRequiredMixin
 
+# Create your views here.\
+
+
+class RegisterView(CreateView):
+    model = User
+    template_name = 'registration.html'
+    form_class = MyUserCreationForm
+
+    def form_valid(self,form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.get_success_url())
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if not next_url:
+            next_url = self.request.POST.get('next')
+        if not next_url:
+            next_url = reverse('webapp:home')
+        return next_url
+
+
+def login_view(request):
+    context = {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('webapp:home')
+        else:
+            context['has_error'] = True
+    return render(request, 'login.html', context=context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('webapp:home')
+
+
+class ProfileView(LoginRequiredMixin, DetailView):
+    model = get_user_model()
+    template_name = "profile.html"
+    paginate_by = 6
+    paginate_orphans = 0
+
+    def get_context_data(self, **kwargs):
+        paginator = Paginator(self.get_object().articles.all(),
+                              self.paginate_by,
+                              self.paginate_orphans)
+        page_number = self.request.GET.get('page', 1)
+        page_object = paginator.get_page(page_number)
+        context = super().get_context_data(**kwargs)
+        context['page_obj'] = page_object
+        context['articles'] = page_object.object_list
+        context['is_paginated'] = page_object.has_other_pages()
+        return context
 # Create your views here.
