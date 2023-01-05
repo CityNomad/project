a039636a9ec4ff97f324e8ba64c92787b24ba7e8
CityNomad <romancheva@hotmail.com>
2023-01-05 23:49:04 +0600

    modified settings of login and logout redirection

--- to_do/to_do/settings.py
+++ to_do/to_do/settings.py
@@ -113,6 +113,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+LOGIN_URL = 'accounts:login'
+LOGIN_REDIRECT_URL = 'webapp:home'
+LOGOUT_REDIRECT_URL = 'webapp:home'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
